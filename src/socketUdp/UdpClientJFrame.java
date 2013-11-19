e4fc4ea0c77f1849f1df2f9ab1cda8116dc70805
yinchuandong <yincd520@sina.com>
2013-11-19 21:54:42 +0800

    修改udpclientjframe

--- src/socketUdp/UdpClientJFrame.java
+++ src/socketUdp/UdpClientJFrame.java
@@ -15,6 +15,7 @@ import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
